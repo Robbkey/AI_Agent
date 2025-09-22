54207704034b0f0e53f041b1c82281567b740aed
Robbkey <mirko.klingels@web.de>
2025-09-22 08:58:35 +0200

    added the call_function to main.py so that the llm can execute given functions

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ import sys
 from dotenv import load_dotenv #command for loading the .env file inside the main directory, so that the api key is usable 
 from google import genai
 from google.genai import types
-#to import the schemas
-from functions.get_files_info import schema_get_files_info
-from functions.get_files_content import schema_get_file_content
-from functions.run_python import schema_run_python_file
-from functions.write_file import schema_write_file
+#to import the schemas and functions
+from functions.get_files_info import schema_get_files_info, get_files_info
+from functions.get_files_content import schema_get_file_content, get_file_content
+from functions.run_python import schema_run_python_file, run_python_file
+from functions.write_file import schema_write_file, write_file
 
 
 def main():
@@ -74,12 +74,70 @@ All paths you provide should be relative to the working directory. You do not ne
     if len(response.function_calls) > 0 :
         #printing the input for each function
         for func in response.function_calls:
-            print(f"Calling function: {func.name}({func.args})")
+
+            #calls a function that allows the llm to use a given function 
+            result = call_function(*response.function_calls)
+
+            #check if any respons was actually produced 
+            if not result.parts[0].function_response.response:
+                raise Exception("something has gone wrong")
+
+            #print the result of the llm using a given function
+            if sys.argv[2] == "--verbose":
+                print(f"-> {result.parts[0].function_response.response}")
+
 
     #if no function was used just print the respons.text
     else:
         print("Response:")
         print(response.text)
+
+
+
+# this is the part of main.py where the llm has a function it wants to use and the script checks 
+#if the function is a valid function and then lets the llm execute the function
+def call_function(function_call_part, verbose=False):
+    #the hard coded work dir
+    work_dircetory = "/home/robbkey/Desktop/BootDev/github.com/Robbkey/AI_Agent/calculator/"
+
+    #this is so that the script can search if there is an actual function for the function_call_part.name that is just a string, so the name of a function
+    func_list = globals()
+
+    #more or less detail
+    if sys.argv[2] == "--verbose":
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else: 
+        print(f" - Calling function: {function_call_part.name}")
+
+    #adding the work dic to the args dic
+    function_call_part.args["working_directory"] = work_dircetory
+    #know we use the func_list dic with the name of the function and the unpacked args dic
+    result = func_list[function_call_part.name](**function_call_part.args)
+
+
+    valid_func = ["get_file_content", "get_files_info", "run_python_file", "write_file"]
+    #the decion which output to creat
+    if function_call_part.name not in valid_func:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_call_part.name,
+                    response={"error": f"Unknown function: {function_call_part.name}"},
+                )
+            ],
+        )
+    else:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_call_part.name,
+                    response={"result": result},
+                )
+            ],
+        )
+
 
 # checking if the script is run with in it self or called by an other
 if __name__ == "__main__":
